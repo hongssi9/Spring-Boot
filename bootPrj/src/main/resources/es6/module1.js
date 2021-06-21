796ca99e1ec793828a1a8edf6d5f7d02942d0376
hongssi9 <vvv9605@naver.com>
2021-06-21 22:32:59 +0900

    비동기, promise, 모듈

---
+++ bootPrj/src/main/resources/es6/module1.js
@@ -0,0 +1,11 @@
+export function test(){
+    console.log("test function");
+}
+
+export default function test1(){
+    console.log("test1 function");
+}
+
+export function test2(){
+    console.log("test2 function");
+}
