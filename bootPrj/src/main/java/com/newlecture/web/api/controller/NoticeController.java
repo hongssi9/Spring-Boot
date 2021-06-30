cc89ee497fc95e330c4e05299246d35ee13beca3
hongssi9 <79443935+hongssi9@users.noreply.github.com>
2021-06-30 23:34:12 +0900

    로컬주소 허용(시큐리티)

--- bootPrj/src/main/java/com/newlecture/web/api/controller/NoticeController.java
+++ bootPrj/src/main/java/com/newlecture/web/api/controller/NoticeController.java
@@ -36,7 +36,7 @@ public class NoticeController {
 	@Autowired //IOC컨테이너에 보관되어있는 객체를 자동으로 생성하는 역할
 	private NoticeService service; //컨테이너에 저장된 서비스 객체를 꺼내서 전역변수로 만들었다.
 	
-	@CrossOrigin(origins = "http://localhost:3000") //3000번으로 오는건 다 허용해준다.
+	@CrossOrigin(origins = "*") //3000번으로 오는건 다 허용해준다.
 	@RequestMapping("list") //GetMapping과 차이는 없다.
 	//@ResponseBody //리턴값을 사용자한테 보여주고 싶을때 그냥 반환만 받을 수 있게	
 	public Map<String, Object> list(
@@ -54,6 +54,14 @@ public class NoticeController {
 		map.put("count", count);
 		
 		return map;
+	}
+	
+	@CrossOrigin(origins = "*") //오리진을 다 붙여줘야하나...귀찮은데
+	@GetMapping("{id}")
+	public Notice get(@PathVariable("id") int id) {
+		
+		
+		return service.get(id);
 	}
 	
 	
